07e6ae2f880b7fc9a323c62e61f08f23f51c9c02
Junow <taristmas@gmail.com>
2020-01-04 01:57:07 +0900

    [Add] Logo 컴포넌트 생성

---
+++ client/src/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogoImage from '../../assets/logo.png';
+
+const Logo: React.FC = () => {
+  return (
+    <>
+      <LogoImage />
+    </>
+  );
+};
+
+export default Logo;
