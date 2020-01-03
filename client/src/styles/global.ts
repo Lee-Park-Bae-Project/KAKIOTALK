20657734143162225c6541fa3a550fb21f6847ba
Junow <taristmas@gmail.com>
2020-01-04 02:48:43 +0900

    [Add] global style 추가

---
+++ client/src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: medium-content-sans-serif-font, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+`;
+
+export const size = {
+  mobile: '375px',
+  tablet: '834px',
+  desktop: '1440',
+};
+
+export const device = {
+  mobile: `(min-width: ${size.mobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
+
+export const color = {
+  YELLO: '#F7CF46',
+  WHITE: '#FEFFFF',
+  BROWN: '#403631',
+  RED: '#DD6F5A',
+  GRAY: '#C6C3C1',
+  BLACK: '#4B4B4B',
+  TEXT_BLACK: '#262525',
+  TEXT_GRAY: '#737373',
+  ROOM_COLOR: '#C0D1DB',
+};
+
+export default Global;
