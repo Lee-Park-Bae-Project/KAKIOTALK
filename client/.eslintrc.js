43728970f0e3c5091777a1aceeb6c3c0fb3e4727
Junow <taristmas@gmail.com>
2020-01-03 20:28:49 +0900

    [Add] 개발 환경 세팅
    prettier, eslint-airbnb

---
+++ client/.eslintrc.js
@@ -0,0 +1,55 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  extends:  [
+    'airbnb-base',
+    // 'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+  ],
+  parserOptions:  {
+  ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+  sourceType:  'module',  // Allows for the use of imports
+  ecmaFeatures:  {
+    jsx:  true,  // Allows for the parsing of JSX
+  },
+  },
+  rules:  {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "comma-dangle": [
+      "error",
+      {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "always-multiline"
+      }
+    ],
+    "comma-style": [
+      "error",
+      "last"
+    ],
+    "array-element-newline": [
+      "error",
+      {
+        "multiline": true,
+        "minItems": 3
+      }
+    ],
+    "object-property-newline": "error"
+  },
+  settings:  {
+    react:  {
+      version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  env: {
+    "browser": true,
+  }
+};
---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <p>
+        <div>
+          hi
+        </div>
+      </p>
+    </div>
+  );
+};
+
+export default App;
