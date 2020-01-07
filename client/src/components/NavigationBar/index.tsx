9e8b18974cf17add018a40827abed8825b8c0371
Junow <taristmas@gmail.com>
2020-01-07 22:05:01 +0900

    [Modified] NavigationBar 의 state 를 상위에서 받도록 변경

--- client/src/components/NavigationBar/index.tsx
+++ client/src/components/NavigationBar/index.tsx
@@ -1,32 +1,37 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import * as S from './styles';
 import FriendTab from './FriendTab';
 import ChatTab from './ChatTab';
 import AddFriend from './AddFriendTab';
 
-const NavigationBar: FC = () => {
-  const [friendSelected, setFriendSelected] = useState(false);
-  const [chatSelected, setChatSelected] = useState(false);
-
-  const friendTabOnClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
-    setFriendSelected(!friendSelected);
-  };
-
-  const chatTabOnClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
-    setChatSelected(!chatSelected);
-  };
-
-  const addFriendAddTabOnClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
-    alert('hihi');
-  };
-
-  return (
+interface NavigationBarProp{
+  /** 친구목록이 선택되었는지 */
+  friendSelected: boolean;
+  /** 채팅목록이 선택되었는지 */
+  chatSelected: boolean;
+  /** 친구목록을 클릭 했을 때 핸들러 */
+  friendTabOnClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  /** 채팅목록을 클릭 했을 때 핸들러 */
+  chatTabOnClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  /** 친구추가를 클릭 했을 때 핸들러 */
+  addFriendTabOnClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+}
+/**
+ * - 네비게이션바
+ * - 친구목록, 채팅목록, 친구추가 기능이 가능
+ * - 친구목록, 채팅목록은 둘중 하나만 렌더링 가능
+ */
+const NavigationBar: FC<NavigationBarProp> = ({
+  friendSelected = true,
+  chatSelected = false,
+  friendTabOnClick,
+  chatTabOnClick,
+  addFriendTabOnClick,
+}) => (
     <S.Container>
       <FriendTab selected={friendSelected} size='small' onClick={friendTabOnClick}/>
       <ChatTab selected={chatSelected} size='small' onClick={chatTabOnClick}/>
-      <AddFriend size='small' onClick={addFriendAddTabOnClick}/>
+      <AddFriend size='small' onClick={addFriendTabOnClick}/>
     </S.Container>
-  );
-};
-
+);
 export default NavigationBar;
