62fbc1ce3ba2677a0bbfc98a34be7d298173a36d
junow <taristmas@gmail.com>
2020-01-17 15:35:17 +0900

    [Add] Hr component 추가

---
+++ client/src/commons/Hr/Hr.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Hr from 'commons/Hr';
+
+export default {
+  title: 'Commons/Hr',
+  component: Hr,
+};
+
+export const defaultHr = () => <Hr/>;
---
+++ client/src/commons/Hr/index.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { color } from 'styles/global';
+
+export default styled.hr`
+  width: 90%;
+  border-color: ${color.GRAY};
+`;
