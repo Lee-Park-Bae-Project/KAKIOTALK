c9cd2e36ed245c97cb1762625769d65844aa736c
junow <taristmas@gmail.com>
2020-01-17 16:46:41 +0900

    [Add] storybook viewport addon 추가

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,9 +1,11 @@
 import { configure, addParameters } from '@storybook/react';
 import { themes } from '@storybook/theming';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 // automatically import all files ending in *.stories.js
 addParameters({
   options: {
     theme: themes.dark,
   },
+  viewport: { viewports: INITIAL_VIEWPORTS },
 });
 configure(require.context('../src', true, /\.stories\.(js|mdx|tsx)$/), module);
