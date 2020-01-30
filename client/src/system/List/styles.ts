179d465b89e406654c95b7841a7b44582589a621
junow <taristmas@gmail.com>
2020-01-30 21:17:47 +0900

    [Modified] css 수정

--- client/src/system/List/styles.ts
+++ client/src/system/List/styles.ts
@@ -3,6 +3,12 @@ import Flex from 'commons/Flex';
 
 export const Container = styled(Flex)`
   flex-direction: column;
-  width: 25rem;
+  justify-content: flex-start;
+  width: 100%;
+  /* max-width: 25rem; */
   height: 100vh;
+`;
+
+export const ItemWrapper = styled(Flex)`
+  width: 100%;
 `;
