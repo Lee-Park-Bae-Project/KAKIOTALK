9c3c4e30950a40933012918e27041b169cf9fb2e
1Consumption <hanseop95@gmail.com>
2020-04-04 20:28:16 +0900

    [Add] 프로젝트 생성

---
+++ iOS/KakioTalk/KakioTalk/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  KakioTalk
+//
+//  Created by 신한섭 on 2020/04/04.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
