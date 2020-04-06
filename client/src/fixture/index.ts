2093004cb74c41223d1077d41438ffe50696fa73
junow <taristmas@gmail.com>
2020-04-07 04:27:16 +0900

    Modified 더미데이터 생성방식 변경

--- client/src/fixture/index.ts
+++ client/src/fixture/index.ts
@@ -1,195 +1,38 @@
 import shortid from 'shortid';
 import faker from 'faker';
+import { ChatRoom } from 'modules/ChatRoomList';
+import { User } from 'modules/userlist';
 
-export const myProfile = {
-  id: shortid.generate(),
-  userName: faker.internet.userName(),
-  statusMessage: faker.lorem.lines(),
+const getChatList = (num: number) => {
+  const list: ChatRoom[] = [];
+  for (let i = 0; i < num; i += 1) {
+    list.push({
+      id: shortid.generate(),
+      userList: [faker.internet.userName(),
+        faker.internet.userName(),
+        faker.internet.userName()],
+      lastMessage: faker.lorem.sentence(),
+      lastModified: Date.now(),
+      numOfNewMessages: Math.round(Math.random() * 100),
+    });
+  }
+  return list;
 };
-export const friendList = [
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
-  {
-    id: shortid.generate(),
-    userName: faker.internet.userName(),
-    statusMessage: faker.lorem.lines(),
-  },
 
-];
+const getUsers = (num: number) => {
+  const list: User[] = [];
+  for (let i = 0; i < num; i += 1) {
+    list.push({
+      id: shortid.generate(),
+      userName: faker.internet.userName(),
+      statusMessage: faker.lorem.lines(),
+    });
+  }
+  return list;
+};
 
-export const chatList = [
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
-  {
-    userList: [faker.internet.userName(),
-      faker.internet.userName(),
-      faker.internet.userName()],
-    lastMessage: faker.lorem.sentence(),
-    lastModified: Date.now(),
-    numOfNewMessage: Math.round(Math.random() * 100),
-  },
+export const [myProfile] = getUsers(1);
+export const friendList = getUsers(15);
 
-];
+
+export const chatList = getChatList(15);
