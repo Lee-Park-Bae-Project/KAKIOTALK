46d0afe05251eddabcf52faf65b78eb21cea4e1d
junow <taristmas@gmail.com>
2020-04-08 00:31:24 +0900

    Add dummy data api

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -1,4 +1,5 @@
 import { Request, NextFunction, Response } from "express";
+
 const get = (req: Request, res: Response, next: NextFunction) => {
   return res.json({ message: "users get" });
 };
---
+++ server/src/routes/v1/dummy.route.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import {
+  getProfile,
+  getFriendList,
+  getChatList,
+} from '../../controllers/dummy.controllers'
+
+const router = express.Router()
+
+router.get('/my-profile', getProfile)
+router.get('/friend-list', getFriendList)
+router.get('/chat-list', getChatList)
+
+export default router
