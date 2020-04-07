7913da1b1b3eb72807d3603f37ae57f59060511a
junow <taristmas@gmail.com>
2020-04-08 02:40:25 +0900

    Add redux-devtools-extension dependency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,9 @@ import Global from 'styles/global';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from 'modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 const App: React.FC = () => (
   <Provider store={store}>
