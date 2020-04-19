c64f2ffefb3ea12623597e7fb3f14c3bbe5be684
1Consumption <hanseop95@gmail.com>
2020-04-20 02:25:23 +0900

    [Add] OAuth client ID 적용하여 정보 받아옴

--- iOS/KakioTalk/KakioTalk/AppDelegate.swift
+++ iOS/KakioTalk/KakioTalk/AppDelegate.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate{
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url)
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GIDSignIn.sharedInstance().clientID = "559423734767-g72rf9iara23u68srlqtm1c0at718jqq.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
     
@@ -20,3 +27,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
+extension AppDelegate: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                print("The user has not signed in before or they have since signed out.")
+            } else {
+                print("\(error.localizedDescription)")
+            }
+            // [START_EXCLUDE silent]
+            NotificationCenter.default.post(
+                name: Notification.Name(rawValue: "ToggleAuthUINotification"), object: nil, userInfo: nil)
+            // [END_EXCLUDE]
+            return
+        }
+        // Perform any operations on signed in user here.
+        guard let user = user else {return}
+        let userId = user.userID                  // For client-side use only!
+        let idToken = user.authentication.idToken // Safe to send to the server
+        let fullName = user.profile.name
+        let givenName = user.profile.givenName
+        let familyName = user.profile.familyName
+        let email = user.profile.email
+        // [START_EXCLUDE]
+        NotificationCenter.default.post(
+            name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+            object: nil,
+            userInfo: ["statusText": user])
+        // [END_EXCLUDE]
+    }
+    // [END signin_handler]
+    // [START disconnect_handler]
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // [START_EXCLUDE]
+        NotificationCenter.default.post(
+            name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+            object: nil,
+            userInfo: ["statusText": "User has disconnected."])
+        // [END_EXCLUDE]
+    }
+}
--- iOS/KakioTalk/KakioTalk/ViewController.swift
+++ iOS/KakioTalk/KakioTalk/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 import GoogleSignIn
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        
+        // Automatically sign in the user.
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(test(_:)), name: Notification.Name(rawValue: "ToggleAuthUINotification"), object: nil)
+    }
+    
+    @objc func test(_ notification: Notification) {
+        guard let user = notification.userInfo?["statusText"] as? GIDGoogleUser else {return}
+        let userId = user.userID                  // For client-side use only!
+        let idToken = user.authentication.idToken // Safe to send to the server
+        let fullName = user.profile.name
+        let givenName = user.profile.givenName
+        let familyName = user.profile.familyName
+        let email = user.profile.email
+        print(userId)
+        print(idToken)
+        print(fullName)
+        print(givenName)
+        print(familyName)
+        print(email)
     }
 }
 
