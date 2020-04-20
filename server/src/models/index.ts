bef64053b780f84c1ee4a9a9797d5279be2502a2
junow <taristmas@gmail.com>
2020-04-21 00:06:35 +0900

    Add Model types

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -9,11 +9,19 @@ import Friend from './friends'
 import Room from './room'
 import RoomParticipants from './roomParticipants'
 
+export type ModelTypes =
+| ReturnType<typeof User>
+| ReturnType<typeof Chat>
+| ReturnType<typeof ChatIsRead>
+| ReturnType<typeof Friend>
+| ReturnType<typeof Room>
+| ReturnType<typeof RoomParticipants>
+
 const config = require('../configs/sequelize.js')[env || 'development']
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
-const models = {
+const models:{[key: string ]: ModelTypes} = {
   User: User(sequelize, DataTypes),
   Chat: Chat(sequelize, DataTypes),
   ChatIsRead: ChatIsRead(sequelize, DataTypes),
