904f322603a516ec4d58d383ce361923ced7e657
junow <taristmas@gmail.com>
2020-04-26 03:57:16 +0900

    Fix timezone

--- client/src/common/utils.ts
+++ client/src/common/utils.ts
@@ -1,7 +1,9 @@
-import moment from 'moment-timezone';
+import moment from 'moment';
+import 'moment-timezone';
 
 moment.locale('ko');
 moment.tz.setDefault('Asia/Seoul');
-export const convertDBTimeTohhmmA = (dbTime: string) => moment(new Date(dbTime), 'MM-DD-YYYY HH:mm:ss').format('hh:mm A');
 
-export const convertMillToMMDDYYYY = (date: number) => moment(date).format('MM-DD-YYYY');
+export const convertDBTimeTohhmmA = (dbTime: string) => moment(new Date(dbTime), 'MM-DD-YYYY HH:mm:ss').tz('Asia/Seoul').format('hh:mm A');
+
+export const convertMillToMMDDYYYY = (date: number) => moment(date).tz('Asia/Seoul').format('MM-DD-YYYY');
