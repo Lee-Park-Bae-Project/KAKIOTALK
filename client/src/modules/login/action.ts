b2658e58527974e90ae69f7e46685e54edc55d23
bbumjun <bumjun7928@naver.com>
2020-05-01 02:28:12 +0900

    login에 필요한 값이 profile type에 명시되지 않아 일단 any type으로  대체

--- client/src/modules/login/action.ts
+++ client/src/modules/login/action.ts
@@ -4,7 +4,7 @@ export const LOGIN_REQUEST = 'login/GET_LOGIN' as const;
 export const GET_LOGIN_SUCCESS = 'login/LOGIN_SUCCESS' as const;
 export const GET_LOGIN_FAILURE = 'login/LOGIN_FAILURE' as const;
 
-export const loginRequest = (profile: Profile) => ({
+export const loginRequest = (profile: any) => ({
   type: LOGIN_REQUEST,
   payload: profile
 });
