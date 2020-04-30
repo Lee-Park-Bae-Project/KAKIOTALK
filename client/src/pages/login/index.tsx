2f8cd62cb9e7e7234a701dfdd3d25e169ceece5d
bbumjun <bumjun7928@naver.com>
2020-05-01 02:32:54 +0900

    props가 필요없다고 판단해 props type제거

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import * as S from 'pages/login/styles';
 import { RouteComponentProps, useHistory } from 'react-router-dom';
-interface Props extends RouteComponentProps {}
 
-const Login: React.FC<RouteComponentProps> = () => (
+const Login: React.FC= () => (
   <S.Container>
     <S.Logo />
     <S.SigninButton>
