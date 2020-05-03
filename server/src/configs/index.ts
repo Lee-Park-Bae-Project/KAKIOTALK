52220347575e923835a83fffeb5fbcc63f3a37ee
marshmello37 <marshmello37@naver.com>
2020-05-03 15:41:00 +0900

    [Chore] 오류수정

--- server/src/configs/index.ts
+++ server/src/configs/index.ts
@@ -1,34 +1,23 @@
-<<<<<<< HEAD
 import { CorsOptions } from 'cors';
-=======
-import { CorsOptions } from 'cors'
-import { CookieOptions } from 'express'
->>>>>>> 8b30815cade8a139431deced16a8ec4f47d40949
+import { CookieOptions } from 'express';
 
 require('dotenv').config();
 
-export const env =
-  process.env.NODE_ENV;
+export const env = process.env.NODE_ENV;
 
-const whiteList = [
-  'http://localhost:3000',
-];
+const whiteList = ['http://localhost:3000'];
 export const corsConfig: CorsOptions = {
   origin: whiteList,
   credentials: true,
-<<<<<<< HEAD
 };
-=======
-}
 
 export const cookieConfig: CookieOptions = {
   httpOnly: true,
   maxAge: 1000 * 60 * 60 * 20,
-}
-export const cookieName = 'kakio_jwt'
+};
+export const cookieName = 'kakio_jwt';
 
 export const jwtConfig = {
   secret: process.env.JWT_SECRET || 'secret',
   ttl: '2h',
-}
->>>>>>> 8b30815cade8a139431deced16a8ec4f47d40949
+};
--- server/src/routes/v1/user.route.ts
+++ server/src/routes/v1/user.route.ts
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 import express from 'express';
 import { userTest } from '../../controllers/user';
-=======
-import express from 'express'
-import { userTest } from '../../controllers/user'
->>>>>>> 8b30815cade8a139431deced16a8ec4f47d40949
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get('/user', userTest);
-=======
-router.get('/user', userTest)
-
-export default router
->>>>>>> 8b30815cade8a139431deced16a8ec4f47d40949
 
 export default router;
