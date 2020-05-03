9709f63b7478424bc11acbcd60ae6c07ef21e8c3
marshmello37 <marshmello37@naver.com>
2020-05-04 02:32:19 +0900

    [Chore] 코드정리

--- client/src/components/Routes/index.tsx
+++ client/src/components/Routes/index.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from 'pages/login';
 import MainContainer from 'containers/MainContainer';
-import ChatRoom from 'pages/chatroom'
+import ChatRoom from 'pages/chatroom';
 const Routes: React.FC = () => (
   <Router>
-    <Route
-        exact
-        path='/'
-        component={MainContainer}
-      />
-      <Route
-        path="/login"
-        component={Login}
-      />
-      <Route path="/chat" component={ChatRoom}/>
-    </Router>
+    <Route exact path="/" component={Login} />
+    <Route path="/main" component={MainContainer} />
+    <Route path="/chat" component={ChatRoom} />
+  </Router>
 );
 
 export default Routes;
