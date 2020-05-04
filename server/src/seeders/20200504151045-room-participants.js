d003459c1b4b600275f0ba2aeff135cea1af165c
junow <taristmas@gmail.com>
2020-05-05 02:22:24 +0900

    Add seed data for leeparkbaeproject, taristmas accounts
    개발용 데이터를 넣어놓기 위해 미리 유저정보를 넣어놓습니다.

--- server/src/seeders/20200504151045-room-participants.js
+++ server/src/seeders/20200504151045-room-participants.js
@@ -1,32 +1,25 @@
 const moment = require('moment')
 
-const tableName = 'users'
-
+const tableName = 'room_participants'
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
-    tableName,
-    [
+    tableName, [
       {
-        id: 1,
-        name: 'Junho Lee',
         uuid: '123',
-        email: 'leeparkbaeproject@gmail.com',
-        status: 'im leeparkbae',
-        googleId: '113283872440363914094',
+        userId: 1,
+        roomId: 1,
         createdAt: moment().format('YYYY-MM-DD hh:mm:ss'),
         updatedAt: moment().format('YYYY-MM-DD hh:mm:ss'),
       },
       {
-        id: 2,
-        name: '이준호',
         uuid: '234',
-        email: 'taristmas@gmail.com',
-        status: 'im junow',
-        googleId: '100916656626485581191',
+        userId: 2,
+        roomId: 1,
         createdAt: moment().format('YYYY-MM-DD hh:mm:ss'),
         updatedAt: moment().format('YYYY-MM-DD hh:mm:ss'),
       },
-    ], {}
+    ]
+
   ),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete(tableName, null, {}),
--- server/src/seeders/20200504164236-friends.js
+++ server/src/seeders/20200504164236-friends.js
@@ -1,32 +1,27 @@
 const moment = require('moment')
 
-const tableName = 'users'
-
+const tableName = 'friends'
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
     tableName,
     [
       {
         id: 1,
-        name: 'Junho Lee',
         uuid: '123',
-        email: 'leeparkbaeproject@gmail.com',
-        status: 'im leeparkbae',
-        googleId: '113283872440363914094',
+        userId: 1,
+        friendId: 1,
         createdAt: moment().format('YYYY-MM-DD hh:mm:ss'),
         updatedAt: moment().format('YYYY-MM-DD hh:mm:ss'),
       },
       {
         id: 2,
-        name: '이준호',
         uuid: '234',
-        email: 'taristmas@gmail.com',
-        status: 'im junow',
-        googleId: '100916656626485581191',
+        userId: 1,
+        friendId: 2,
         createdAt: moment().format('YYYY-MM-DD hh:mm:ss'),
         updatedAt: moment().format('YYYY-MM-DD hh:mm:ss'),
       },
-    ], {}
+    ]
   ),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete(tableName, null, {}),
