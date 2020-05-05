a602b3eb408b8a52cc541755bd720b2cd269e3c6
marshmello37 <marshmello37@naver.com>
2020-05-06 03:10:15 +0900

    [Add] logout 추가

--- client/src/containers/MainContainer.tsx
+++ client/src/containers/MainContainer.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'modules';
-import { Route } from 'react-router-dom';
 import Main from 'pages/main';
 import { getFriends } from 'modules/friends';
 import { getChatRoom } from 'modules/chatRoom';
 import { getProfile } from 'modules/profile';
-import Login from 'pages/login';
-
+import request from 'common/request';
+import { useHistory } from 'react-router-dom';
 import withAuth, { Props } from 'hocs/withAuth';
 
 const { useState, useEffect } = React;
@@ -17,6 +16,7 @@ const MainContainer: React.FC<Props> = ({ name, email, uuid }) => {
   const friendList = useSelector((state: RootState) => state.friends);
   const chatList = useSelector((state: RootState) => state.chatRoomList);
   const loginState = useSelector((state: RootState) => state.login);
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(getProfile());
@@ -53,7 +53,17 @@ const MainContainer: React.FC<Props> = ({ name, email, uuid }) => {
 
   const logoutTabOnClick = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-  ): void => {};
+  ): void => {
+    request
+      .getLogout()
+      .then(response => {
+        console.log(response);
+        history.push('/');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   const [searchKeyword, setSearchKeyword] = useState('');
   const onSearchKeywordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
--- server/src/routes/v1/auth.route.ts
+++ server/src/routes/v1/auth.route.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getUserInfo, login } from '../../controllers/auth';
+import { getUserInfo, login, logout } from '../../controllers/auth';
 
 import isAuth from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.post('/google/', login);
+router.post('/logout', logout);
 router.get('/check-auth', isAuth, getUserInfo);
 
 export default router;
