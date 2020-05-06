419d3560e748e6b1aa5d0b956509f0e40dd7d22b
bbumjun <bumjun7928@naver.com>
2020-05-06 16:02:00 +0900

    [chore] code restoration

--- client/src/pages/main/Friend/index.tsx
+++ client/src/pages/main/Friend/index.tsx
@@ -15,30 +15,54 @@ interface Props {
   myProfile: User;
   friendList: User[];
 }
+const Friend: FC<Props> = ({ myProfile, friendList }) => {
+  const [popup, setPopup] = useState(false);
+  const [clickedUser, setClickedUser] = useState({
+    id: '',
+    userName: '',
+    statusMessage: '',
+  });
+  const onProfileClose = () => {
+    setPopup(false);
+  };
+  return (
+    <List>
+      <UserCard
+        key={myProfile.id}
+        userName={myProfile.userName}
+        statusMessage={myProfile.statusMessage}
+      />
+      <Hr />
+      친구 {friendList.length}
+      {friendList.map(({ id, statusMessage, userName }) => {
+        const onUserCardClick = () => {
+          setPopup(true);
+          setClickedUser({
+            id: id,
+            userName: userName,
+            statusMessage: statusMessage,
+          });
+        };
 
-const Friend: FC<Props> = ({ myProfile, friendList }) => (
-  <List>
-    <UserCard
-      key={myProfile.id}
-      userName={myProfile.userName}
-      statusMessage={myProfile.statusMessage}
-    />
-    <Hr />
-    {myProfile.userName}'s friends {friendList.length}
-    {friendList.map(({ id, statusMessage, userName }) => {
-      const onUserCardClick = () => {
-        alert(userName);
-      };
-      return (
-        <UserCard
-          key={id}
-          userName={userName}
-          statusMessage={statusMessage}
-          onClick={onUserCardClick}
+        return (
+          <UserCard
+            key={id}
+            userName={userName}
+            statusMessage={statusMessage}
+            onClick={onUserCardClick}
+          />
+        );
+      })}
+      {popup ? (
+        <Profile
+          id={clickedUser.id}
+          userName={clickedUser.userName}
+          statusMessage={clickedUser.statusMessage}
+          onRemoveClick={onProfileClose}
         />
-      );
-    })}
-  </List>
-);
+      ) : null}
+    </List>
+  );
+};
 
 export default Friend;
