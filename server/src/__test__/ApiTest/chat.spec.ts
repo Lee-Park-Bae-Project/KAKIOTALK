fb9310455da61ba7f5da20ecf8a96f45fe00b5ad
junow <taristmas@gmail.com>
2020-05-11 02:02:51 +0900

    Add ApiTest - chat

---
+++ server/src/__test__/ApiTest/chat.spec.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+
+const app = require('../../app')
+
+const roomId = 1
+describe(`GET: /v1/chat/${roomId}`, () => {
+  it('roomId 로 채팅 조회 | 200', async () => {
+    const response = await request(app).get(`/v1/chat/${roomId}`)
+    expect(response.status).toEqual(200)
+  })
+
+  it('roomId 로 채팅 조회', async () => {
+    const response = await request(app).get('/v1/chat/aaa')
+    expect(response.status).toEqual(404)
+  })
+})
