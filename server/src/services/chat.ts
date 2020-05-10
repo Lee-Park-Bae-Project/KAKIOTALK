dc15617e3565b216ed1cf88897eb1c8c018e727c
junow <taristmas@gmail.com>
2020-05-11 01:00:35 +0900

    Add getChtsByRoomid service

--- server/src/services/chat.ts
+++ server/src/services/chat.ts
@@ -1,19 +1,21 @@
 import { models } from '../models'
 
 export const findRoomById = (roomId: string) => models.Room.findOne({ where: { roomId } })
-export const getChatsByRoomId = (roomId: string) => models.Chat.findAll({
-  where: { roomId },
-  include: [
-    {
-      model: models.RoomParticipants,
-      as: 'sender',
-      include: [
-        models.User,
-      ],
-    },
-    {
-      model: models.Room,
-      as: 'room',
-    },
-  ],
-})
+export const getChatsByRoomId = (roomId: string) => models.Chat.findAll({ include: [
+  {
+    model: models.RoomParticipants,
+    where: { roomId },
+    as: 'chatInfo',
+    include: [
+      {
+        model: models.User,
+        as: 'sender',
+      },
+      {
+        model: models.Room,
+        as: 'roomInfo',
+      },
+    ],
+  },
+] })
+
