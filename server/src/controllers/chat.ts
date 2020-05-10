42f0d7e3b768870e1a25edd028c77c631d040637
junow <taristmas@gmail.com>
2020-05-11 02:01:42 +0900

    Add error handling logic in getChats

--- server/src/controllers/chat.ts
+++ server/src/controllers/chat.ts
@@ -1,7 +1,7 @@
 import {
   NextFunction, Request, Response,
 } from 'express'
-
+import createError from 'http-errors'
 import * as chatService from '../services/chat'
 import { response } from '../common/utils'
 
@@ -9,6 +9,9 @@ export const getChats = async (req: Request, res: Response, next: NextFunction) 
   try {
     const { roomId } = req.params
     const chats = await chatService.getChatsByRoomId(roomId)
+    if (chats.length === 0) {
+      return next(createError(404, '방 정보가 없습니다.'))
+    }
     response(res, chats)
   } catch (e) {
     next(e)
