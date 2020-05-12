db1bb4539c029c2f5c9c959f1d90af20b4aa79b7
junow <taristmas@gmail.com>
2020-05-12 16:38:05 +0900

    Modify api test

--- server/src/__test__/ApiTest/chat.spec.ts
+++ server/src/__test__/ApiTest/chat.spec.ts
@@ -5,12 +5,13 @@ const app = require('../../app')
 const roomId = 1
 describe(`GET: /v1/chat/${roomId}`, () => {
   it('roomId 로 채팅 조회 | 200', async () => {
-    const response = await request(app).get(`/v1/chat/${roomId}`)
+    const response = await request(app).get(`/v1/chat/chat/${roomId}`)
     expect(response.status).toEqual(200)
   })
 
-  it('roomId 로 채팅 조회', async () => {
+  it('roomId 로 채팅 조회 | 404', async () => {
     const response = await request(app).get('/v1/chat/aaa')
     expect(response.status).toEqual(404)
   })
 })
+
