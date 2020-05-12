7d24f0018c9bc2b9c3fb1b0ecf1f3e3a40e3ac27
junow <taristmas@gmail.com>
2020-05-12 16:37:18 +0900

    Modify export user service

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,9 +1,9 @@
 import { models } from '../models'
 
-const findByGoogleId = (googleId: string) => models.User.findOne({ where: { googleId } })
-const createUser = (googleId: string) => models.User.create({ googleId })
+export const findByGoogleId = (googleId: string) => models.User.findOne({ where: { googleId } })
+export const createUser = (googleId: string) => models.User.create({ googleId })
 
-const findOrCreate = (
+export const findOrCreate = (
   googleId: string,
   name: string,
   email: string,
@@ -20,14 +20,14 @@ const findOrCreate = (
   },
 
 )
-const setAccessToken = (googleId: string, accessToken: string) => models.User.update(
+export const setAccessToken = (googleId: string, accessToken: string) => models.User.update(
   { accessToken },
   {
     where: { googleId },
     returning: true,
   },
 )
-const setUserInfo = (googleId: string, name: string, email: string) => models.User.update(
+export const setUserInfo = (googleId: string, name: string, email: string) => models.User.update(
   {
     name,
     email,
@@ -38,10 +38,3 @@ const setUserInfo = (googleId: string, name: string, email: string) => models.Us
   },
 )
 
-export default {
-  findByGoogleId,
-  createUser,
-  setAccessToken,
-  setUserInfo,
-  findOrCreate,
-}
