3da76ca411d64c2a5f9827a8d675405c6b6e5e82
marshmello37 <marshmello37@naver.com>
2020-05-14 11:20:50 +0900

    [chore] public url 소문자로 수정

--- client/src/components/Routes/index.tsx
+++ client/src/components/Routes/index.tsx
@@ -5,9 +5,9 @@ import MainContainer from 'containers/MainContainer';
 import ChatRoom from 'pages/chatroom';
 import { configs } from 'common/constants';
 const Routes: React.FC = () => (
-  <Router basename={configs.PUBLIC_URL}>
-    <Route exact path="/" component={Login} />
-    <Route path="/login" component={MainContainer} />
+  <Router>
+    <Route exact path="/" component={MainContainer} />
+    <Route path="/login" component={Login} />
     <Route path="/chat" component={ChatRoom} />
   </Router>
 );
