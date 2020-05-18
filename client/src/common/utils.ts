8d3e997d914570dfb6216fa9ebc1e65ee8384c24
junow <taristmas@gmail.com>
2020-05-19 01:15:26 +0900

    Add getTimeDBFormat util function and test

--- client/src/common/utils.ts
+++ client/src/common/utils.ts
@@ -3,6 +3,7 @@ import 'moment-timezone';
 
 moment.locale('ko');
 const tzSeoul = 'Asia/Seoul';
+const DB_TIME_FORMAT = 'YYYY-MM-DD hh:mm:ss';
 
 export const convertDBTimeTohhmmA = (dbTime: string) => {
   const seoul = moment.tz(new Date(dbTime), tzSeoul);
@@ -13,3 +14,16 @@ export const convertMillToMMDDYYYY = (date: number) => {
   const seoul = moment.tz(date, tzSeoul);
   return seoul.format('MM-DD-YYYY');
 };
+
+export const getCurTimeDBFormat = () => {
+  const date = new Date();
+  return moment
+    .utc(date)
+    .tz(tzSeoul)
+    .format(DB_TIME_FORMAT);
+};
+
+export const getCurTimeDBFormatForTest = (date: Date) => moment
+  .utc(date)
+  .tz(tzSeoul)
+  .format(DB_TIME_FORMAT);
