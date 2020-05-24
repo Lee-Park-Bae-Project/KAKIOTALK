cba106d86ea8fe96a917b2fe5a654de9d8a1bcc8
bbumjun <bumjun7928@naver.com>
2020-05-24 16:27:50 +0900

    [modify] util 모듈에 error message 작성 및 적용

--- server/src/controllers/social.ts
+++ server/src/controllers/social.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import createError from 'http-errors';
-import { response } from '../common/utils';
+import { response, message } from '../common/utils';
 import * as userService from '../services/user';
 import socialService from '../services/social';
-import { IDecodedUser, IUser } from 'src/types';
 
 const getFriendsList = async (
   req: Request,
@@ -12,23 +11,23 @@ const getFriendsList = async (
 ) => {
   try {
     const { googleId } = req.decodedUser;
-    const user:IUser|null= await userService.findByGoogleId(googleId);
-    let data : any 
-    if(user){
-      data = await socialService.getFriendsList(user.id);
+    const user = await userService.findByGoogleId(googleId);
+    if (!user) {
+      throw createError(401, { message: message.INVALID_GOOGLE_ID });
     }
-    else {
-      throw(createError(401,{message:'계정이 유효하지 않습니다.'}))
+    const data = await socialService.getFriendsList(user.id);
+    if (!data) {
+      throw createError(401, { message: message.INVALID_FRIEND_ID });
     }
-    const friendlist = data.friend.map((friend) =>{ 
-      const {uuid,name,email,statusMessage} = friend.user
-      return ({
-      uuid,
-      name,
-      email,
-      statusMessage,
-    })
-  });
+    const friendlist = data.friend.map((friend) => {
+      const { uuid, name, email, statusMessage } = friend.user;
+      return {
+        uuid,
+        name,
+        email,
+        statusMessage,
+      };
+    });
     response(res, friendlist);
   } catch (e) {
     next(e);
@@ -36,41 +35,47 @@ const getFriendsList = async (
 };
 const addFriend = async (req: Request, res: Response, next: NextFunction) => {
   try {
-
-    const user: IUser|null = await userService.findByGoogleId(req.decodedUser.googleId);
+    const user = await userService.findByGoogleId(req.decodedUser.googleId);
     const friendEmail: string = req.body.email;
-    const friend: IUser|null = await userService.findByEmail(friendEmail);
-    if (friend&&user) {
-      await socialService.addFriend(user.id, friend.id);
-      const {uuid,email,name,statusMessage} = friend
-      response(res, {
-        uuid,
-        email,
-        name,
-        statusMessage
-      });
-    } else {
-      return next(createError(401, {message:'유효하지 않은 이메일입니다.'}));
+    const friend = await userService.findByEmail(friendEmail);
+    if (!friend || !user) {
+      return next(createError(401, { message: message.INVALID_EMAIL }));
     }
+    const [, created] = await socialService.addFriend(user.id, friend.id);
+    if (!created) {
+      return next(createError(401, { message: message.ALREADY_EXIST_FRIEND }));
+    }
+    const { uuid, email, name, statusMessage } = friend;
+    response(res, {
+      uuid,
+      email,
+      name,
+      statusMessage,
+    });
   } catch (e) {
     next(e);
   }
 };
-const deleteFriend =async (req:Request,res:Response,next:NextFunction)=>{
-  try{
-    const user:IUser|null = await userService.findByGoogleId(req.decodedUser.googleId)
-    const deleteUser:IUser|null = await userService.findByUuid(req.body.uuid);
-    if(!user||!deleteUser) {
-      throw(createError(401,{message:'유효하지 않은 계정입니다.'}))
+const deleteFriend = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await userService.findByGoogleId(req.decodedUser.googleId);
+    const deleteUser = await userService.findByUuid(req.body.uuid);
+    if (!user || !deleteUser) {
+      throw createError(401, { message: message.INVALID_FRIEND_ID });
     }
-      const deleted = await socialService.deleteFriend(user.id,deleteUser.id)
-      if(deleted==0) {
-        throw(createError(401,{message:"삭제할 친구의 계정이 유효하지 않습니다."}))
-      }
-      response(res,{uuid:deleteUser.uuid})
-    
-  }catch (e) {
+    const deleted = await socialService.deleteFriend(user.id, deleteUser.id);
+    if (deleted == 0) {
+      throw createError(401, {
+        message: message.ERROR_OCCURED,
+      });
+    }
+    response(res, { uuid: deleteUser.uuid });
+  } catch (e) {
     next(e);
   }
-}
-export { getFriendsList, addFriend ,deleteFriend};
+};
+export { getFriendsList, addFriend, deleteFriend };
--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -3,31 +3,17 @@ import {
 } from 'express'
 import createError from 'http-errors'
 import { models } from '../models'
-import { response } from '../common/utils'
+import { response ,message} from '../common/utils'
 import * as userService from '../services/user'
 import { IUser } from 'src/types'
-
-const userTest = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const user = await models.User.create({
-      id: `${Date.now()}`,
-      name: 'name',
-      email: '123',
-    })
-
-    res.status(200).json(user)
-  } catch (e) {
-    next(e)
-  }
-}
 
 const getMyProfile = async (req:Request, res:Response, next:NextFunction) => {
   try {
     if (!req.decodedUser) {
-      return next(createError(401, '로그인 필요'))
+      return next(createError(401,message.LOGIN_REQUIRED ))
     }
-    const user:IUser|null = await userService.findByGoogleId(req.decodedUser.googleId)
-    if(!user) throw(createError(401,'유효하지 않은 계정입니다.'))
+    const user = await userService.findByGoogleId(req.decodedUser.googleId)
+    if(!user) throw(createError(401,message.INVALID_GOOGLE_ID))
     const {
       uuid, email, name, statusMessage,
     } = user
@@ -43,5 +29,5 @@ const getMyProfile = async (req:Request, res:Response, next:NextFunction) => {
   }
 }
 export {
-  userTest, getMyProfile,
+   getMyProfile,
 }
