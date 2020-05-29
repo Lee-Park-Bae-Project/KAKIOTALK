7c1e3363eb99da0819d3320090f100c83d191ba2
bbumjun <bumjun7928@naver.com>
2020-05-29 16:38:50 +0900

    [chore] 에러메시지 수정 / login url 상수 추가 / 불필요한 로그 삭제

--- client/src/hocs/withAuth.tsx
+++ client/src/hocs/withAuth.tsx
@@ -3,21 +3,19 @@ import React, { useState, useEffect } from 'react';
 import request from 'common/request';
 import { RouteComponentProps } from 'react-router';
 import { AxiosError } from 'axios';
-
 export interface WithAuthProps {
   name: string;
   email: string;
   uuid: string;
-  statusMessage : string;
+  statusMessage: string;
 }
-
 function withAuth<T extends WithAuthProps>(Component: React.ComponentType<T>) {
   return (props: T & RouteComponentProps) => {
     const [userInfo, setNewProps] = useState({
       name: '',
       email: '',
       uuid: '',
-      statusMessage:'',
+      statusMessage: '',
     });
 
     useEffect(() => {
@@ -25,11 +23,10 @@ function withAuth<T extends WithAuthProps>(Component: React.ComponentType<T>) {
         request
           .getUserInfo()
           .then(response => {
-            const { name, email, uuid ,statusMessage} = response.data.data;
-            setNewProps({ name, email, uuid,statusMessage });
+            const { name, email, uuid, statusMessage } = response.data.data;
+            setNewProps({ name, email, uuid, statusMessage });
           })
           .catch((e: AxiosError) => {
-            console.log(e);
             props.history.push('/login');
           });
       })();
--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,29 +1,21 @@
-import {
-  NextFunction, Request, Response,
-} from 'express'
-import jwt from 'jsonwebtoken'
-import { IDecodedUser } from '../types'
-import {
-  cookieName,
-  jwtConfig,
-} from '../configs'
-
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { IDecodedUser } from '../types';
+import { cookieName, jwtConfig } from '../configs';
+import { message } from '../common/utils';
 const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.cookies[cookieName]
+    const token = req.cookies[cookieName];
     if (!token) {
-      throw new Error('no token')
+      throw new Error(message.LOGIN_REQUIRED);
     }
-    const decoded = jwt.verify(
-      token,
-      jwtConfig.secret,
-    )
+    const decoded = jwt.verify(token, jwtConfig.secret);
 
-    req.decodedUser = decoded as IDecodedUser
-    next()
+    req.decodedUser = decoded as IDecodedUser;
+    next();
   } catch (e) {
-    next(e)
+    next(e);
   }
-}
+};
 
-export default isAuthenticated
+export default isAuthenticated;
