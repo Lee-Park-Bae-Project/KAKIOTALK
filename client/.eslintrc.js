55e75c4a2e7f690654b57fa761b534c435996018
junow <taristmas@gmail.com>
2020-05-31 18:30:08 +0900

    Modify eslint, prettier

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,76 +1,90 @@
-module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-  extends:  [
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
     'airbnb-base',
     // 'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
     // 'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'plugin:import/typescript',
     'plugin:react/recommended',
   ],
-  parserOptions:  {
-  ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-  sourceType:  'module',  // Allows for the use of imports
-  ecmaFeatures:  {
-    jsx:  true,  // Allows for the parsing of JSX
-  },
-  },
-  rules:  {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    "react/prefer-stateless-function": 0,
-    "react/jsx-filename-extension": 0,
-    "react/jsx-one-expression-per-line": 0,
-    "comma-dangle": [
-      "error",
-      {
-        "arrays": "always-multiline",
-        "objects": "always-multiline",
-        "imports": "always-multiline",
-        "exports": "always-multiline",
-        "functions": "ignore"
-      }
-    ],
-    "comma-style": [
-      "error",
-      "last"
-    ],
-    // "function-paren-newline": ["error", { "minItems": 3 }],
-    "array-element-newline": [
-      "error",
-      {
-        "multiline": true,
-        "minItems": 3
-      }
-    ],
-    "object-property-newline": "error",
-    "react/prop-types": 0,
-    "@typescript-eslint/explicit-function-return-type":0,
-    "import/no-extraneous-dependencies": 0,
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-  ],
-  "@typescript-eslint/interface-name-prefix": 0,
-  },
-  settings:  {
-    react:  {
-      version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
     },
-    "import/resolver": {
-      "node": {
-        "paths": ["src"],
-      }
-    }
+  },
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
+    'comma-style': ['error', 'last'],
+    'no-trailing-spaces': 'error',
+    // "function-paren-newline": ["error", { "minItems": 3 }],
+    'object-property-newline': [
+      'error',
+      {
+        allowAllPropertiesOnSameLine: false,
+      },
+    ],
+    'no-unused-vars': 'off',
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
+    'no-empty-function': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    'no-duplicate-imports': 'error',
+    'object-curly-newline': [
+      'error',
+      {
+        minProperties: 2,
+      },
+    ],
+    'object-property-newline': 'error',
+    'react/prop-types': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'import/no-extraneous-dependencies': 0,
+    semi: ['error', 'never'],
+    'import/extensions': [
+      'off',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'eol-last': ['error', 'always'],
+    'import/no-unresolved': 'off',
+    '@typescript-eslint/member-delimiter-style': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
   },
   env: {
-    "browser": true,
-    "jest": true,
-  }
-};
+    browser: true,
+    jest: true,
+  },
+}
