3b3ab231d0bfc05cd16ef3fcbebc4486c128498d
bbumjun <bumjun7928@naver.com>
2020-05-31 18:29:38 +0900

    add update profile logic in database

--- server/src/routes/v1/user.route.ts
+++ server/src/routes/v1/user.route.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { getMyProfile} from '../../controllers/user';
-import isAuth from '../../middlewares/auth'
+import { getMyProfile, updateProfile } from '../../controllers/user';
+import isAuth from '../../middlewares/auth';
 const router = express.Router();
-router.use(isAuth)
-router.get('/my-profile',getMyProfile)
+router.use(isAuth);
+router.get('/my-profile', getMyProfile);
+router.patch('/update-profile', updateProfile);
 export default router;
--- server/src/services/user.ts
+++ server/src/services/user.ts
@@ -1,18 +1,23 @@
-import { models } from '../models'
+import { models } from '../models';
 
-export const findByGoogleId = (googleId: string) => models.User.findOne({ where: { googleId } })
-export const findByUuid = (uuid: string) => models.User.findOne({ where: { uuid } })
-export const createUser = (googleId: string) => models.User.create({ googleId })
-export const findByAccessToken = (accessToken:string) => models.User.findOne({ where: { accessToken } })
-export const findByEmail = (email:string) => models.User.findOne({ where: { email } })
-export const findById = (id:number) => models.User.findOne({ where: { id } })
+export const findByGoogleId = (googleId: string) =>
+  models.User.findOne({ where: { googleId } });
+export const findByUuid = (uuid: string) =>
+  models.User.findOne({ where: { uuid } });
+export const createUser = (googleId: string) =>
+  models.User.create({ googleId });
+export const findByAccessToken = (accessToken: string) =>
+  models.User.findOne({ where: { accessToken } });
+export const findByEmail = (email: string) =>
+  models.User.findOne({ where: { email } });
+export const findById = (id: number) => models.User.findOne({ where: { id } });
 export const findOrCreate = (
   googleId: string,
   name: string,
   email: string,
   googleAccessToken: string
-) => models.User.findOrCreate(
-  {
+) =>
+  models.User.findOrCreate({
     where: { googleId },
     defaults: {
       googleId,
@@ -20,24 +25,26 @@ export const findOrCreate = (
       email,
       googleAccessToken,
     },
-  },
-
-)
-export const setAccessToken = (googleId: string, accessToken: string) => models.User.update(
-  { accessToken },
-  {
-    where: { googleId },
-    returning: true,
-  },
-)
-export const setUserInfo = (googleId: string, name: string, email: string) => models.User.update(
-  {
-    name,
-    email,
-  },
-  {
-    where: { googleId },
-    returning: true,
-  },
-)
-
+  });
+export const setAccessToken = (googleId: string, accessToken: string) =>
+  models.User.update(
+    { accessToken },
+    {
+      where: { googleId },
+      returning: true,
+    }
+  );
+export const setUserInfo = (
+  googleId: string,
+  name: string,
+  statusMessage: string
+) =>
+  models.User.update(
+    {
+      name,
+      statusMessage,
+    },
+    {
+      where: { googleId },
+    }
+  );
