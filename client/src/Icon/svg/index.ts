2886d8b98b04f3c72bf1a55547d59dd1b18f743e
marshmello37 <marshmello37@naver.com>
2020-06-01 22:42:52 +0900

    [chore] travis 수정

--- client/src/Icon/svg/index.ts
+++ client/src/Icon/svg/index.ts
@@ -6,11 +6,8 @@ export { ReactComponent as Chat } from './chat.svg';
 export { ReactComponent as ChatFilled } from './chat-filled.svg';
 export { ReactComponent as AddFriend } from './person-add.svg';
 export { ReactComponent as Account } from './account.svg';
-<<<<<<< HEAD
 export { ReactComponent as Logout } from './logout.svg';
 export { ReactComponent as AddChat } from './add-chat.svg';
-=======
 export { ReactComponent as logout } from './logout.svg';
 export { ReactComponent as EditProfile } from './edit-profile.svg';
 export { ReactComponent as Confirm } from './confirm.svg';
->>>>>>> 27b6975b72af8c524deaf1139b4e8705824cdf21
--- server/.prettierrc.js
+++ server/.prettierrc.js
@@ -1,16 +1,8 @@
 module.exports = {
   semi: false,
-<<<<<<< HEAD
-  //trailingComma: 'all',
-=======
   trailingComma: 'all',
->>>>>>> 27b6975b72af8c524deaf1139b4e8705824cdf21
   singleQuote: true,
   jsxSingleQuote: true,
   printWidth: 80,
   tabWidth: 2,
-<<<<<<< HEAD
-};
-=======
 }
->>>>>>> 27b6975b72af8c524deaf1139b4e8705824cdf21
