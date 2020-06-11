6122417da57d6724f1ee5c873559484de4e483ea
marshmello37 <marshmello37@naver.com>
2020-06-11 17:19:46 +0900

    [chore] delete /KAKIOTALK

--- packages/server/src/configs/index.ts
+++ packages/server/src/configs/index.ts
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 export const env = process.env.NODE_ENV || 'development'
 
-const whiteList = ['http://localhost:3000','https://lee-park-bae-project.github.io/KAKIOTALK/']
+const whiteList = ['http://localhost:3000','https://lee-park-bae-project.github.io']
 export const corsConfig: CorsOptions = {
   origin: whiteList,
   credentials: true,
