81a56b8d87f0ae50c8f13a61a2d6de688d8d1c92
bbumjun <bumjun7928@naver.com>
2020-06-13 04:17:46 +0900

    [modify] tab selector 제거하고 main content를 중첩 라우팅

--- packages/client/src/pages/main/index.tsx
+++ packages/client/src/pages/main/index.tsx
@@ -1,54 +1,28 @@
-import React, {
-  FC, useState,
-} from 'react'
+import React, { FC } from 'react'
 import * as S from 'pages/main/styles'
 import NavigationBar from 'components/NavigationBar'
-import { RoomState } from 'modules/room'
 import { RootState } from 'modules'
 import { Loader } from 'components'
 import FriendContainer from 'containers/FriendContainer'
 import Room from 'system/Room'
 import { useSelector } from 'react-redux'
+import { Route } from 'react-router-dom'
 
-interface Props {
-  roomState: RoomState
-}
-
-const Main: FC<Props> = ({ roomState }) => {
+const Main: FC = () => {
   const { isLoggedIn } = useSelector((state: RootState) => state.login)
-  const [tabSelector, setTabSelector] = useState({
-    friend: true,
-    chat: false,
-  })
 
-  const friendTabOnClick = () => {
-    setTabSelector({
-      friend: true,
-      chat: false,
-    })
-  }
-  const chatTabOnClick = () => {
-    setTabSelector({
-      friend: false,
-      chat: true,
-    })
-  }
   return (
     <S.Container>
       {isLoggedIn ? (
         <S.MainWrapper>
-          {tabSelector.friend && <FriendContainer />}
-          {tabSelector.chat && <Room roomState={roomState} />}
+          <Route path='/main/friend-list' component={FriendContainer}/>
+          <Route path='/main/chat-list' component={Room}/>
         </S.MainWrapper>
       ) : (
         <Loader/>
       )}
       <S.NavigationBarWrapper>
-        <NavigationBar
-          tabSelector={tabSelector}
-          friendTabOnClick={friendTabOnClick}
-          chatTabOnClick={chatTabOnClick}
-        />
+        <NavigationBar/>
       </S.NavigationBarWrapper>
 
     </S.Container>
