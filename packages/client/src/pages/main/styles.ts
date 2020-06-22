61124d5dacc411ac7e792c6b31e4c8d84b533d0c
bbumjun <bumjun7928@naver.com>
2020-06-22 13:45:32 +0900

    chore fix profile style

--- packages/client/src/pages/main/styles.ts
+++ packages/client/src/pages/main/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   width:100%;
   height:100%;
+  overscroll-behavior-y: contain;
 `
 
 export const MainWrapper = styled.div`
