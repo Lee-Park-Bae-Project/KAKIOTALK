15504270a6fd782f7b72243b0e02fe4ef9d17d06
bbumjun <bumjun7928@naver.com>
2020-06-23 16:25:54 +0900

    fix routing url

--- packages/client/src/components/Routes/index.tsx
+++ packages/client/src/components/Routes/index.tsx
@@ -9,8 +9,7 @@ import { url } from 'common/constants'
 
 const Routes: React.FC = () => (
   <Router>
-    <Route path={url.main.friendList} component={MainContainer} />
-    <Route path={url.main.chatList} component={MainContainer} />
+    <Route path={url.main.default} component={MainContainer} />
     <Route path={url.login} component={Login} />
     <Route path={`${url.room}/:roomUuid`} component={ChatRoomContainer} />
     <Redirect from='/' to={url.main.friendList}/>
