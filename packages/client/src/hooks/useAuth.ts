211fdaee80ba5034dc8076b5cfa41b3e71efd864
bbumjun <bumjun7928@naver.com>
2020-07-10 04:16:47 +0900

    fix useAuth hooks

--- packages/client/src/hooks/useAuth.ts
+++ packages/client/src/hooks/useAuth.ts
@@ -1,40 +1,18 @@
-import {
-  useEffect, useState,
-} from 'react'
+import { useEffect } from 'react'
 import * as request from 'common/request'
-import { AxiosError } from 'axios'
 import { url } from 'common/constants'
-import { useDispatch } from 'react-redux'
-import {
-  loginFailure, loginSuccess,
-} from 'modules/login'
+
 import { useHistory } from 'react-router-dom'
 
 const useAuth = () => {
-  const dispatch = useDispatch()
   const history = useHistory()
-  const [isLoaded, setLoaded] = useState(false)
-  const [isLoggedIn, setLogin] = useState(false)
-  const [userInfo, setUserInfo] = useState({
-    name: '',
-    email: '',
-    uuid: '',
-    statusMessage: '',
-    imageUrl: '',
-  })
   useEffect(() => {
-    request.getUserInfo().then((response) => {
-      setLoaded(true)
-      setLogin(true)
-      setUserInfo(response.data.data)
-    }).catch((e: AxiosError) => {
-      setLoaded(true)
-      setLogin(false)
-    })
-  })
-  return {
-    isLoaded, isLoggedIn, userInfo,
-  }
+    (async () => {
+      request.getUserInfo().catch(() => {
+        history.push(url.login)
+      })
+    })()
+  }, [])
 }
 
 export default useAuth
