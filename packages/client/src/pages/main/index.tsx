2d9e50048a8599988078ffc016d2d2a5872547b6
bbumjun <bumjun7928@naver.com>
2020-07-12 18:13:39 +0900

    add loader in main page

--- packages/client/src/pages/main/index.tsx
+++ packages/client/src/pages/main/index.tsx
@@ -2,7 +2,9 @@ import React, {
   FC, useEffect,
 } from 'react'
 import * as S from 'pages/main/styles'
-import NavigationBar from 'components/NavigationBar'
+import {
+  Loader, NavigationBar,
+} from 'components'
 import Room from 'system/Room'
 import { useDispatch } from 'react-redux'
 import { Route } from 'react-router-dom'
@@ -10,31 +12,32 @@ import { url } from 'common/constants'
 import { getRoomRequest } from 'modules/room'
 import { afterLogin } from 'socket'
 import Friend from 'pages/main/Friend'
-import { getUserInfo } from 'common/request'
 import { useAuth } from 'hooks'
 
 const Main: FC = () => {
   const dispatch = useDispatch()
-  useAuth()
+  const {
+    isLoggedIn, userInfo,
+  } = useAuth()
+  const { uuid } = userInfo
   useEffect(() => {
     dispatch(getRoomRequest())
-    getUserInfo().then((response) => {
-      const { uuid } = response.data.data
+    if (isLoggedIn) {
       afterLogin({ uuid })
-    })
+    }
   }, [])
   window.history.pushState(null, '', window.location.href)
-  return (
-    <S.Container>
-        <S.MainWrapper>
-          <Route path={url.main.friendList} component={Friend}/>
-          <Route path={url.main.chatList}component={Room}/>
-        </S.MainWrapper>
+  return (isLoggedIn
+    ? <S.Container>
+      <S.MainWrapper>
+        <Route path={url.main.friendList} component={Friend} />
+        <Route path={url.main.chatList} component={Room} />
+      </S.MainWrapper>
       <S.NavigationBarWrapper>
-        <NavigationBar/>
+        <NavigationBar />
       </S.NavigationBarWrapper>
-
     </S.Container>
+    : <Loader/>
   )
 }
 
