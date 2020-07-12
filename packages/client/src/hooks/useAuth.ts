cdab52feacb88211fc7058b1114ffd43dc815270
bbumjun <bumjun7928@naver.com>
2020-07-12 18:12:55 +0900

    return login status & user inf o in auth hook

--- packages/client/src/hooks/useAuth.ts
+++ packages/client/src/hooks/useAuth.ts
@@ -1,4 +1,6 @@
-import { useEffect } from 'react'
+import {
+  useEffect, useState,
+} from 'react'
 import * as request from 'common/request'
 import { url } from 'common/constants'
 
@@ -6,13 +8,33 @@ import { useHistory } from 'react-router-dom'
 
 const useAuth = () => {
   const history = useHistory()
+  const [isLoggedIn, setLogin] = useState(false)
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    email: '',
+    uuid: '',
+    statusMessage: '',
+    imageUrl: '',
+  })
   useEffect(() => {
     (async () => {
-      request.getUserInfo().catch(() => {
-        history.push(url.login)
+      request.getUserInfo().then((res) => {
+        setLogin(true)
+        const {
+          name, email, uuid, statusMessage, imageUrl,
+        } = res.data.data
+        setUserInfo({
+          name, email, uuid, statusMessage, imageUrl,
+        })
       })
+        .catch(() => {
+          history.push(url.login)
+        })
     })()
   }, [])
+  return {
+    isLoggedIn, userInfo,
+  }
 }
 
 export default useAuth
