f61f122852edbabb311cf90a122b8e7603589be5
bbumjun <bumjun7928@naver.com>
2020-07-26 19:02:35 +0900

    modify loader css

--- packages/client/src/components/Loader/index.tsx
+++ packages/client/src/components/Loader/index.tsx
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 
 const S = { Loader: styled.div`
   margin: 1rem auto;
-  border: 0.5rem solid #f3f3f3;
+  border: 0.5rem solid rgba(255,255,255,1);
   border-radius: 50%;
   border-top: 0.5rem solid #F7CF46;
   width: 5rem;
   height: 5rem;
-  -webkit-animation: spin 1s  infinite; /* Safari */
-  animation: spin 1s  infinite;
+  -webkit-animation: spin 0.9s  infinite; /* Safari */
+  animation: spin 0.9s  infinite;
 
   @keyframes spin {
     0% {
