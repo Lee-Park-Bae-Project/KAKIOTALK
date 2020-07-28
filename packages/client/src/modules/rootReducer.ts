9d665b1e7c51434ba75586809eb2fd81fc60d99c
bbumjun <bumjun7928@naver.com>
2020-07-28 19:21:05 +0900

    rootReducer에서 logout시에 모든 redux state 초기화로직 삭제

--- packages/client/src/modules/rootReducer.ts
+++ packages/client/src/modules/rootReducer.ts
@@ -3,7 +3,7 @@ import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage/session'
 import friends from 'modules/friends'
 import profile from 'modules/profile'
-import login, { LoginAction, LOGOUT_REQUEST } from 'modules/login'
+import login from 'modules/login'
 import room from 'modules/room'
 import chat from 'modules/chat'
 
@@ -13,19 +13,12 @@ const persistConfig = {
   blacklist: [],
 
 }
-const appReducer = combineReducers({
+const rootReducer = combineReducers({
   friends,
   profile,
   login,
   room,
   chat,
 })
-
-const rootReducer = (state: any, action: LoginAction) => {
-  if (action.type === LOGOUT_REQUEST) {
-    state = undefined
-  }
-  return appReducer(state, action)
-}
 
 export default persistReducer(persistConfig, rootReducer)
