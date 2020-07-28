6cdc40f5b71940d9bcefa5179ae331e7aab4efcc
bbumjun <bumjun7928@naver.com>
2020-07-28 19:22:15 +0900

    logout success 이후 redux초기화 로직 login saga로 이동 & 초기화 함수들 한번의 yield으로 묶음

--- packages/client/src/modules/sagas/loginSaga.ts
+++ packages/client/src/modules/sagas/loginSaga.ts
@@ -1,30 +1,39 @@
 import {
-  call, put, takeEvery,
+  all, call, put, takeLatest,
 } from 'redux-saga/effects'
 import {
   LOGIN_REQUEST,
   loginFailure,
   loginRequest,
   loginSuccess,
+  LOGOUT_REQUEST,
+  logoutSuccess,
 } from 'modules/login'
-import { getProfile } from 'modules/profile'
-import { getFriends } from 'modules/friends'
-import { getRoomRequest } from 'modules/room'
+import { getProfile, resetProfile } from 'modules/profile'
+import { getFriends, resetFriends } from 'modules/friends'
+import { getRoomRequest, resetRoom } from 'modules/room'
 import { alert } from 'common/utils'
 import * as request from 'common/request'
 
 function* loginRequestSaga({ payload }: ReturnType<typeof loginRequest>) {
   try {
     yield call(request.getLogin, payload)
-    yield put(loginSuccess())
-    yield put(getProfile())
-    yield put(getFriends())
-    yield put(getRoomRequest())
+    yield* [put(loginSuccess()), put(getProfile()), put(getFriends()), put(getRoomRequest())]
   } catch (e) {
     yield put(loginFailure(e))
     alert.error(e.response.data.data.message)
   }
 }
+
+function* logoutRequestSaga() {
+  try {
+    yield call(request.getLogout)
+    yield* [put(logoutSuccess()), put(resetProfile()), put(resetFriends()), put(resetRoom())]
+  } catch (e) {
+    alert.error(e.response.data.data.message)
+  }
+}
 export default function* loginSaga() {
-  yield takeEvery(LOGIN_REQUEST, loginRequestSaga)
+  yield takeLatest(LOGIN_REQUEST, loginRequestSaga)
+  yield takeLatest(LOGOUT_REQUEST, logoutRequestSaga)
 }
