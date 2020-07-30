7ee38700c5df87da40aee8a5b12e1b33c9da0d07
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-30 13:14:28 +0000

    Deploy Lee-Park-Bae-Project/KAKIOTALK to github.com/Lee-Park-Bae-Project/KAKIOTALK.git:gh-pages

---
+++ precache-manifest.11a53ae67c8447857783849ab39bbd93.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47d35ace885791a3faa892b9b5c09a5",
+    "url": "/KAKIOTALK/index.html"
+  },
+  {
+    "revision": "64684a33109ee2c81605",
+    "url": "/KAKIOTALK/static/js/2.2a9626fb.chunk.js"
+  },
+  {
+    "revision": "abdafba50be0f238f07891d3165e8988",
+    "url": "/KAKIOTALK/static/js/2.2a9626fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29ff9d08b3c632da430",
+    "url": "/KAKIOTALK/static/js/main.2e50a13a.chunk.js"
+  },
+  {
+    "revision": "0406f24752d97f5d5a0d",
+    "url": "/KAKIOTALK/static/js/runtime-main.5ac0ff15.js"
+  },
+  {
+    "revision": "f322cb5a8241ef68dfdcaf7811c565da",
+    "url": "/KAKIOTALK/static/media/account.f322cb5a.svg"
+  },
+  {
+    "revision": "95a46151caf2550da82be2575a6b881c",
+    "url": "/KAKIOTALK/static/media/arrow_down.95a46151.svg"
+  },
+  {
+    "revision": "5cea32b1f9b274a4fe2d10ed43b7c1db",
+    "url": "/KAKIOTALK/static/media/arrow_left.5cea32b1.svg"
+  },
+  {
+    "revision": "47f91d3c18c094447076b81f340ea15e",
+    "url": "/KAKIOTALK/static/media/arrow_up.47f91d3c.svg"
+  },
+  {
+    "revision": "013855fdc682cf229084cfec90bfda42",
+    "url": "/KAKIOTALK/static/media/chat-filled.013855fd.svg"
+  },
+  {
+    "revision": "cbb2af310b14f0b4a0085d96bc2d24ad",
+    "url": "/KAKIOTALK/static/media/chat-start.cbb2af31.svg"
+  },
+  {
+    "revision": "0ec68f32e56d9b9837b8135c646f32e5",
+    "url": "/KAKIOTALK/static/media/chat.0ec68f32.svg"
+  },
+  {
+    "revision": "613d650effcede9e66ad2a056beb6758",
+    "url": "/KAKIOTALK/static/media/check.613d650e.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/KAKIOTALK/static/media/close.777b93c6.svg"
+  },
+  {
+    "revision": "4f7c935656413570c0c67076f0b9e5fb",
+    "url": "/KAKIOTALK/static/media/confirm.4f7c9356.svg"
+  },
+  {
+    "revision": "57ab24788b805833b5bf0578870e95f8",
+    "url": "/KAKIOTALK/static/media/delete.57ab2478.svg"
+  },
+  {
+    "revision": "2df2b9a86300ea420135d88d65bfbc58",
+    "url": "/KAKIOTALK/static/media/edit-profile.2df2b9a8.svg"
+  },
+  {
+    "revision": "3d22011bf713c7455788bf6b0e7f4aba",
+    "url": "/KAKIOTALK/static/media/edit.3d22011b.svg"
+  },
+  {
+    "revision": "2049a84d0e9749c63af9b0bf482bb626",
+    "url": "/KAKIOTALK/static/media/logo.2049a84d.png"
+  },
+  {
+    "revision": "a39c383bb7ce8dc31925eceeed051368",
+    "url": "/KAKIOTALK/static/media/logout.a39c383b.svg"
+  },
+  {
+    "revision": "55f6849429de279d473c459c7ead3249",
+    "url": "/KAKIOTALK/static/media/menu.55f68494.svg"
+  },
+  {
+    "revision": "53e064ac2464dbc4070c1b583788da12",
+    "url": "/KAKIOTALK/static/media/person-add.53e064ac.svg"
+  },
+  {
+    "revision": "25cabb353b79433fc1bdfc921de120b7",
+    "url": "/KAKIOTALK/static/media/person-filled.25cabb35.svg"
+  },
+  {
+    "revision": "5578747622b03bd9365f185120673090",
+    "url": "/KAKIOTALK/static/media/person.55787476.svg"
+  },
+  {
+    "revision": "b627ea0283a5e542e6fae48473f9cf4c",
+    "url": "/KAKIOTALK/static/media/quit.b627ea02.svg"
+  },
+  {
+    "revision": "5f3eac93c91349af6df3a3a694dd2e9c",
+    "url": "/KAKIOTALK/static/media/search.5f3eac93.svg"
+  },
+  {
+    "revision": "5be1f33130382cb7da32e61179f99059",
+    "url": "/KAKIOTALK/static/media/send.5be1f331.svg"
+  }
+]);
